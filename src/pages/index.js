fc0dbc0440d8309a77c02e78babeaf0d0aa19e83
cmmerritt <christiane.merritt@gmail.com>
2021-10-25 21:40:55 -0700

    fix background color to cover page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import '../styles/index-styles.sass';
 const IndexPage = () => {
   return (
     <main>
-      <title>Blair Community Garden</title>
-      <h1>Welcome to Blair Community Garden!</h1>
-      <p>A site for Blair gardeners. More coming soon!</p>
+      <body>
+        <title>Blair Community Garden</title>
+        <h1>Welcome to Blair Community Garden!</h1>
+        <p>A site for Blair gardeners. More coming soon!</p>
+      </body>
     </main>
   )
 }
